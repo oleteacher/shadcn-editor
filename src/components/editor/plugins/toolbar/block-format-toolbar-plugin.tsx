dbf8f069290ef6bbd5f451594700036cd8b76941
Talha Mujahid <htmujahid@gmail.com>
2024-11-28 12:13:05 +0500

    refact: format toolbar plugins

--- src/components/editor/plugins/toolbar/block-format-toolbar-plugin.tsx
+++ src/components/editor/plugins/toolbar/block-format-toolbar-plugin.tsx
@@ -1,180 +1,25 @@
-import { $createCodeNode } from '@lexical/code'
-import {
-  $isListNode,
-  INSERT_CHECK_LIST_COMMAND,
-  INSERT_ORDERED_LIST_COMMAND,
-  INSERT_UNORDERED_LIST_COMMAND,
-  ListNode,
-} from '@lexical/list'
-import {
-  $createHeadingNode,
-  $createQuoteNode,
-  $isHeadingNode,
-} from '@lexical/rich-text'
-import { HeadingTagType } from '@lexical/rich-text'
-import { $setBlocksType } from '@lexical/selection'
+import { $isListNode, ListNode } from '@lexical/list'
+import { $isHeadingNode } from '@lexical/rich-text'
 import { $findMatchingParent, $getNearestNodeOfType } from '@lexical/utils'
-import {
-  $createParagraphNode,
-  $isRootOrShadowRoot,
-  BaseSelection,
-  RangeSelection,
-} from 'lexical'
-import { $getSelection } from 'lexical'
-import { $isRangeSelection } from 'lexical'
-import { LexicalEditor } from 'lexical'
-import {
-  CodeIcon,
-  Heading1Icon,
-  Heading2Icon,
-  Heading3Icon,
-  Heading4Icon,
-  Heading5Icon,
-  Heading6Icon,
-  ListIcon,
-  ListOrderedIcon,
-  ListTodoIcon,
-  QuoteIcon,
-  TextIcon,
-} from 'lucide-react'
+import { $isRootOrShadowRoot, BaseSelection, $isRangeSelection } from 'lexical'
 
+import { useToolbarContext } from '@/components/editor/context/toolbar-context'
+import { useUpdateToolbarHandler } from '@/components/editor/hooks/use-update-toolbar'
 import {
   Select,
   SelectContent,
   SelectGroup,
-  SelectItem,
   SelectTrigger,
 } from '@/components/ui/select'
 
-import { useToolbarContext } from '../../context/toolbar-context'
-import { useUpdateToolbarHandler } from '../../hooks/use-update-toolbar'
+import { blockTypeToBlockName } from './block-format/block-format-data'
 
-export function BlockFormatDropDown() {
+export function BlockFormatDropDown({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   const { activeEditor, blockType, setBlockType } = useToolbarContext()
-
-  const formatParagraph = () => {
-    activeEditor.update(() => {
-      const selection = $getSelection()
-      if ($isRangeSelection(selection)) {
-        $setBlocksType(selection, () => $createParagraphNode())
-      }
-    })
-  }
-
-  const formatHeading = (headingSize: HeadingTagType) => {
-    if (blockType !== headingSize) {
-      activeEditor.update(() => {
-        const selection = $getSelection()
-        $setBlocksType(selection, () => $createHeadingNode(headingSize))
-      })
-    }
-  }
-
-  const formatBulletList = () => {
-    if (blockType !== 'bullet') {
-      activeEditor.dispatchCommand(INSERT_UNORDERED_LIST_COMMAND, undefined)
-    } else {
-      formatParagraph()
-    }
-  }
-
-  const formatCheckList = () => {
-    if (blockType !== 'check') {
-      activeEditor.dispatchCommand(INSERT_CHECK_LIST_COMMAND, undefined)
-    } else {
-      formatParagraph()
-    }
-  }
-
-  const formatNumberedList = () => {
-    if (blockType !== 'number') {
-      activeEditor.dispatchCommand(INSERT_ORDERED_LIST_COMMAND, undefined)
-    } else {
-      formatParagraph()
-    }
-  }
-
-  const formatQuote = () => {
-    if (blockType !== 'quote') {
-      activeEditor.update(() => {
-        const selection = $getSelection()
-        $setBlocksType(selection, () => $createQuoteNode())
-      })
-    }
-  }
-
-  const formatCode = () => {
-    if (blockType !== 'code') {
-      activeEditor.update(() => {
-        let selection = $getSelection()
-
-        if (selection !== null) {
-          if (selection.isCollapsed()) {
-            $setBlocksType(selection, () => $createCodeNode())
-          } else {
-            const textContent = selection.getTextContent()
-            const codeNode = $createCodeNode()
-            selection.insertNodes([codeNode])
-            selection = $getSelection()
-            if ($isRangeSelection(selection)) {
-              selection.insertRawText(textContent)
-            }
-          }
-        }
-      })
-    }
-  }
-
-  const blockTypeToBlockName: Record<
-    string,
-    { label: string; icon: React.ReactNode; handler: () => void }
-  > = {
-    paragraph: {
-      label: 'Normal',
-      icon: <TextIcon className="size-4" />,
-      handler: formatParagraph,
-    },
-    h1: {
-      label: 'Heading 1',
-      icon: <Heading1Icon className="size-4" />,
-      handler: () => formatHeading('h1'),
-    },
-    h2: {
-      label: 'Heading 2',
-      icon: <Heading2Icon className="size-4" />,
-      handler: () => formatHeading('h2'),
-    },
-    h3: {
-      label: 'Heading 3',
-      icon: <Heading3Icon className="size-4" />,
-      handler: () => formatHeading('h3'),
-    },
-    number: {
-      label: 'Numbered List',
-      icon: <ListOrderedIcon className="size-4" />,
-      handler: formatNumberedList,
-    },
-    bullet: {
-      label: 'Bulleted List',
-      icon: <ListIcon className="size-4" />,
-      handler: formatBulletList,
-    },
-    check: {
-      label: 'Check List',
-      icon: <ListTodoIcon className="size-4" />,
-      handler: formatCheckList,
-    },
-    code: {
-      label: 'Code Block',
-      icon: <CodeIcon className="size-4" />,
-      handler: formatCode,
-    },
-    quote: {
-      label: 'Quote',
-      icon: <QuoteIcon className="size-4" />,
-      handler: formatQuote,
-    },
-  }
 
   function $updateToolbar(selection: BaseSelection) {
     if ($isRangeSelection(selection)) {
@@ -233,20 +78,7 @@ export function BlockFormatDropDown() {
         <span>{blockTypeToBlockName[blockType].label}</span>
       </SelectTrigger>
       <SelectContent>
-        <SelectGroup>
-          {Object.entries(blockTypeToBlockName).map(([key, value]) => (
-            <SelectItem
-              key={key}
-              value={key}
-              onPointerDown={() => value.handler()}
-            >
-              <div className="flex items-center gap-1 font-normal">
-                {value.icon}
-                {value.label}
-              </div>
-            </SelectItem>
-          ))}
-        </SelectGroup>
+        <SelectGroup>{children}</SelectGroup>
       </SelectContent>
     </Select>
   )
--- src/components/editor/plugins/toolbar/toolbar-plugin.tsx
+++ src/components/editor/plugins/toolbar/toolbar-plugin.tsx
@@ -4,10 +4,17 @@ import { useState } from 'react'
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext'
 import { COMMAND_PRIORITY_CRITICAL, SELECTION_CHANGE_COMMAND } from 'lexical'
 
+import { ToolbarContext } from '@/components/editor/context/toolbar-context'
 import { Separator } from '@/components/ui/separator'
 
-import { ToolbarContext } from '../../context/toolbar-context'
 import { BlockFormatDropDown } from './block-format-toolbar-plugin'
+import { FormatBulletedList } from './block-format/format-bulleted-list'
+import { FormatCheckList } from './block-format/format-check-list'
+import { FormatCodeBlock } from './block-format/format-code-block'
+import { FormatHeading } from './block-format/format-heading'
+import { FormatNumberedList } from './block-format/format-numbered-list'
+import { FormatParagraph } from './block-format/format-paragraph'
+import { FormatQuote } from './block-format/format-quote'
 import { BlockInsertPlugin } from './block-insert-plugin'
 import { ClearFormattingToolbarPlugin } from './clear-formatting-toolbar-plugin'
 import { CodeLanguageToolbarPlugin } from './code-language-toolbar-plugin'
@@ -50,7 +57,15 @@ export function ToolbarPlugin() {
       <div className="vertical-align-middle sticky top-0 z-10 flex gap-2 overflow-auto border-b bg-white p-1">
         <HistoryToolbarPlugin />
         <Separator orientation="vertical" className="h-8" />
-        <BlockFormatDropDown />
+        <BlockFormatDropDown>
+          <FormatParagraph />
+          <FormatHeading levels={['h1', 'h2', 'h3']} />
+          <FormatNumberedList />
+          <FormatBulletedList />
+          <FormatCheckList />
+          <FormatCodeBlock />
+          <FormatQuote />
+        </BlockFormatDropDown>
         {blockType === 'code' ? (
           <CodeLanguageToolbarPlugin />
         ) : (
