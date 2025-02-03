6e163a274ec80e5442d5bbacc248cfd237c0e587
Susan <oleteacher@users.noreply.github.com>
2025-02-03 16:50:10 -0600

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -1,12 +1,12 @@
 export const siteConfig = {
   name: "shadcn/editor",
-  url: "https://shadcn-editor.vercel.app",
-  ogImage: "https://shadcn-editor.vercel.app/og.jpg",
+  url: "https://shadcn-editor-delta.vercel.app/",
+  ogImage: "https://shadcn-editor-delta.vercel.app/og.jpg",
   description:
     "Lexical based text editor using shadcn/ui. Accessible. Customizable. Open Source.",
   links: {
-    twitter: "https://twitter.com/htmujahid",
-    github: "https://github.com/htmujahid/shadcn-editor",
+    twitter: "https://aicrafter.us",
+    github: "https://github.com/codinginbarn/",
   },
 }
 
